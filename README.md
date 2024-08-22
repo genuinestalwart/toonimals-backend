a3c2484942fd2c8e85f5cdc5fcf0a7dbf9fa1371
genuinestalwart <genuinestalwart@gmail.com>
2024-08-22 15:37:51 +0600

    added basic GET apis

---
+++ README.md
@@ -0,0 +1,9 @@
+# Toonimals Backend
+
+This is the backend code of [https://github.com/genuinestalwart/toonimals-frontend](https://github.com/genuinestalwart/toonimals-frontend)
+
+If you want to run this project locally, please follow these steps:
+
+- git clone this project to download the code first.
+- create an `.env` file where you will store `DB_USERNAME`, `DB_PASSWORD` and `DB_CLUSTER`.
+- then, do `npm install` in the terminal and `npm run dev` to run the project.
---
+++ index.js
@@ -0,0 +1,58 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+app.get("/", (req, res) => {
+	res.redirect("https://toonimals-frontend.vercel.app/");
+});
+
+const run = async () => {
+	try {
+		// await client.connect();
+		const animalsColl = client.db("ToonimalsDB").collection("animals");
+		const categoriesColl = client
+			.db("ToonimalsDB")
+			.collection("categories");
+
+		//
+
+		app.get("/categories", async (req, res) => {
+			const result = await categoriesColl.find().toArray();
+			res.send(result);
+		});
+
+		app.get("/animals", async (req, res) => {
+			const { category } = req.query;
+
+			const query = category
+				? { category: { $regex: category, $options: "i" } }
+				: {};
+
+			const result = await animalsColl.find(query).toArray();
+			res.send(result);
+		});
+	} finally {
+		// await client.close();
+	}
+};
+
+run();
+
+app.listen(port, () => {
+	console.log(`Listening to port ${port}`);
+});
